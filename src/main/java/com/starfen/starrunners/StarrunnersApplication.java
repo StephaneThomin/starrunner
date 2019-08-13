498a08c82210d5b9f43a99ffe11bef0aff80c10a
Stephane Thomin <thomin.stephane@gmail.com>
2019-08-13 17:57:01 +0200

    first page

---
+++ src/main/java/com/starfen/starrunners/StarrunnersApplication.java
@@ -0,0 +1,13 @@
+package com.starfen.starrunners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarrunnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarrunnersApplication.class, args);
+	}
+
+}
